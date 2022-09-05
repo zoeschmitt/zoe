1f2d853c36501748f0655cab8410426bc1157095
Zoe Schmitt <zoeschmitt@hotmail.com>
2022-09-05 14:58:37 -0500

    feat: added text to tech stack section

--- src/sections/tech/index.tsx
+++ src/sections/tech/index.tsx
@@ -1,18 +1,26 @@
+import Col from "../../components/col";
 import Row from "../../components/row";
 import "./tech.scss";
 
 const Tech = () => {
   return (
-    <section>
-      <Row className="tech">
-        <img src="assets/js.svg" alt="javascript" />
-        <img src="assets/ts.svg" alt="typescript" />
-        <img src="assets/swift.svg" alt="swift programming language" />
-        <img src="assets/python.svg" alt="python programming language" />
-        <img src="assets/flutter.svg" alt="flutter" />
-        <img src="assets/react.svg" alt="react js" />
-        <img src="assets/angular.svg" alt="angular" />
-      </Row>
+    <section className="tech">
+      <Col>
+        <h2>My Tech Stack</h2>
+        <p>
+          Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem
+          ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum{" "}
+        </p>
+        <Row>
+          <img src="assets/js.svg" alt="javascript" />
+          <img src="assets/ts.svg" alt="typescript" />
+          <img src="assets/swift.svg" alt="swift programming language" />
+          <img src="assets/python.svg" alt="python programming language" />
+          <img src="assets/flutter.svg" alt="flutter" />
+          <img src="assets/react.svg" alt="react js" />
+          <img src="assets/angular.svg" alt="angular" />
+        </Row>
+      </Col>
     </section>
   );
 };
