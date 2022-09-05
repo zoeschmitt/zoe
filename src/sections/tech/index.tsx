0df1c1a972bea030eb966a7b67ab07ff865f6425
Zoe Schmitt <zoeschmitt@hotmail.com>
2022-09-05 14:06:10 -0500

    fix: updated tech stack icons to improve resolution

--- src/sections/tech/index.tsx
+++ src/sections/tech/index.tsx
@@ -3,15 +3,17 @@ import "./tech.scss";
 
 const Tech = () => {
   return (
-    <Row className="tech">
-      <img src="assets/js.svg" alt="javascript" />
-      <img src="assets/ts.svg" alt="typescript" />
-      <img src="assets/swift.svg" alt="swift programming language" />
-      <img src="assets/python.svg" alt="python programming language" />
-      <img src="assets/flutter.svg" alt="flutter" />
-      <img src="assets/react.svg" alt="react js" />
-      <img src="assets/angular.svg" alt="angular" />
-    </Row>
+    <section>
+      <Row className="tech">
+        <img src="assets/js.svg" alt="javascript" />
+        <img src="assets/ts.svg" alt="typescript" />
+        <img src="assets/swift.svg" alt="swift programming language" />
+        <img src="assets/python.svg" alt="python programming language" />
+        <img src="assets/flutter.svg" alt="flutter" />
+        <img src="assets/react.svg" alt="react js" />
+        <img src="assets/angular.svg" alt="angular" />
+      </Row>
+    </section>
   );
 };
 
