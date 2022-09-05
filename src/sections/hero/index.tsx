f52e7884ca9b7e59792c3abb6d2c1d9c61797515
Zoe Schmitt <zoeschmitt@hotmail.com>
2022-09-05 14:25:59 -0500

    feat: added more about me button to hero

--- src/sections/hero/index.tsx
+++ src/sections/hero/index.tsx
@@ -1,3 +1,4 @@
+import Button from "../../components/button";
 import Col from "../../components/col";
 import "./hero.scss";
 
@@ -8,6 +9,9 @@ const Hero = () => {
         <Col>
           <h1 className="title">hi, i'm zoe</h1>
           <p className="subtitle">software engineer + designer</p>
+          <div>
+            <Button>more about me</Button>
+          </div>
         </Col>
         <div className="end">
           <div className="circle" aria-hidden="true">
