294f7ada92185aea963becfd1f95d88d8fbffd23
Zoe Schmitt <zoeschmitt@hotmail.com>
2022-09-05 20:25:26 -0500

    fix: improved global use of hover and focus animations/outlines

--- src/components/side-content/index.tsx
+++ src/components/side-content/index.tsx
@@ -7,26 +7,18 @@ const CSS_PREFIX = "side";
 type Props = {
   children?: React.ReactNode;
   right?: boolean;
-  email?: boolean;
   className?: string;
 };
 
 const SideContent = (props: Props) => {
-  const { children, right, email, className } = props;
+  const { children, right, className } = props;
   return (
     <div
       className={classnames(CSS_PREFIX, className, {
         [`${CSS_PREFIX}--right`]: right,
       })}
     >
-      <div
-        className={classnames({
-          email: email,
-          socials: !email,
-        })}
-      >
-        {children}
-      </div>
+      <div className="socials">{children}</div>
     </div>
   );
 };
