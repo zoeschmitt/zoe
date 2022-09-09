524be9ca05fd2ee6a69462ed835d3a04d2abc7ea
Zoe Schmitt <zoeschmitt@hotmail.com>
2022-09-09 18:38:04 -0500

    feat: moved circle div into its own component

---
+++ src/components/circle/index.tsx
@@ -0,0 +1,12 @@
+import './circle.scss';
+
+const Circle = () => {
+  return (
+    <div className="circle" aria-hidden="true">
+      <img src="assets/skills.svg" alt="" />
+      <p className="emoji">👩🏼‍💻</p>
+    </div>
+  );
+};
+
+export default Circle;
