67bddcca0bfec04271e49ec9d8d85897af3c50f7
Zoe Schmitt <zoeschmitt@hotmail.com>
2022-09-09 18:39:17 -0500

    fix: tech section content changes, responsiveness updates

--- src/sections/tech/index.tsx
+++ src/sections/tech/index.tsx
@@ -3,14 +3,16 @@ import "./tech.scss";
 
 const Tech = () => {
   return (
-    <section id="tech" className="tech">
+    <section id="skills" className="tech">
       <Col className="wrapper">
         <Col className="text">
           <h2>tech I work with</h2>
           <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
-            ad minim veniam.
+            You'll usually find me building web apps with JavaScript, TypeScript, React, and
+            Angular. For mobile development, I enjoy using Flutter, Swift, and
+            Java to build cross-platform apps. Occasionally, I dabble in back-end
+            development with Node, Express, AWS, and MongoDB. I also have
+            intermediate experience with machine learning and web3 using Python.
           </p>
         </Col>
         <div className="grid">
