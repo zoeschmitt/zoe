38ff71c784fc23ce0ddd7c86ac9faf1d2800a0ed
Zoe Schmitt <zoeschmitt@hotmail.com>
2022-09-09 18:40:12 -0500

    chore: removed circle from hero

--- src/sections/hero/index.tsx
+++ src/sections/hero/index.tsx
@@ -5,24 +5,17 @@ import "./hero.scss";
 const Hero = () => {
   return (
     <section id="hero" className="hero">
-      <div className="hero-grid">
-        <Col>
-          <h1 className="title">hi, i'm zoe</h1>
-          <p className="subtitle">software engineer + designer</p>
-          <Button
-            onClick={() => document.getElementById("about")?.scrollIntoView()}
-            aria-label="Go to about me section"
-          >
-            more about me
-          </Button>
-        </Col>
-        <div className="end">
-          <div className="circle" aria-hidden="true">
-            <img src="assets/skills.svg" alt="" />
-            <p className="emoji">👩🏼‍💻</p>
-          </div>
-        </div>
-      </div>
+      <Col>
+        <h1 className="title">hi, i'm zoe</h1>
+        <p className="subtitle">software engineer + designer</p>
+        <Button
+          onClick={() => document.getElementById("about")?.scrollIntoView()}
+          aria-label="Go to about me section"
+        >
+          about me
+        </Button>
+        <img className="arrow" src="assets/big_arrow.svg" alt="" />
+      </Col>
     </section>
   );
 };
