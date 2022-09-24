8e7baaf18950c2c51ea38b70572a007bcbe34e8a
Zoe Schmitt <zoeschmitt@hotmail.com>
2022-09-24 18:45:36 -0500

    fix: iterating over tech stack for DRY

--- src/sections/tech/index.tsx
+++ src/sections/tech/index.tsx
@@ -1,4 +1,6 @@
 import Col from "../../components/col";
+import classnames from "classnames";
+import tech from "../../data/stack";
 import "./tech.scss";
 
 const Tech = () => {
@@ -8,39 +10,24 @@ const Tech = () => {
         <Col className="text">
           <h2>tech I work with</h2>
           <p>
-            You'll usually find me building web apps with JavaScript, TypeScript, React, and
-            Angular. For mobile development, I enjoy using Flutter, Swift, and
-            Java to build cross-platform apps. Occasionally, I dabble in back-end
-            development with Node, Express, AWS, and MongoDB. I also have
-            intermediate experience with machine learning and web3 using Python.
+            You'll usually find me building web apps with JavaScript,
+            TypeScript, React, and Angular. For mobile development, I enjoy
+            using Flutter, Swift, and Java to build cross-platform apps.
+            Occasionally, I dabble in back-end development with Node, Express,
+            AWS, and MongoDB. I also have intermediate experience with machine
+            learning and web3 using Python.
           </p>
         </Col>
         <div className="grid">
-          <img
-            src="assets/logos/js.svg"
-            alt="javascript"
-            className="item item--vert"
-          />
-
-          <img
-            src="assets/logos/flutter.svg"
-            alt="flutter"
-            className="item item--vert"
-          />
-          <img
-            src="assets/logos/react.svg"
-            alt="react js"
-            className="item item--vert"
-          />
-        </div>
-        <div className="grid">
-          <img
-            src="assets/logos/swift_h.svg"
-            alt="swift programming language"
-            className="item"
-          />
-          <img src="assets/logos/ts.svg" alt="typescript" className="item" />
-          <img src="assets/logos/angular.svg" alt="angular" className="item" />
+          {tech.map((i) => (
+            <img
+              src={`assets/logos/${i.src}`}
+              alt={i.alt}
+              className={classnames("item", {
+                "item--vert": i.vert,
+              })}
+            />
+          ))}
         </div>
       </Col>
     </section>
---
+++ src/types/tech-img.ts
@@ -0,0 +1,7 @@
+type TechImg = {
+  src: string;
+  alt: string;
+  vert?: boolean;
+};
+
+export default TechImg;
